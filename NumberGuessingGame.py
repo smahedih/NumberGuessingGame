9b814da0e58649e9bb800403016d3f0d66a080df
Mahedi Hasan <20787093+smahedih@users.noreply.github.com>
2025-11-20 13:48:42 +0100

    Update NumberGuessingGame.py

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,10 +1,11 @@
 from random import randint
 
 guessingNumber = int(input("Enter your guessing number between 1 and 10: "))
-randomNumber = randint(1, 11)
+randomNumber = randint(1, 10)
 
 if guessingNumber == randomNumber:
     print("Hurrah! You won.")
 else:
     print("Oops! You lost.")
     print("Random number was: ", randomNumber)
+
